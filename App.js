3b21c54c43d917a5d985bdb9471ab5961269d2ac
Jully Nixon <jully.niixon@gmail.com>
2021-11-03 13:53:30 -0300

    Atividade: Core components.

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, Image, Input, Button } from './src/components';
+import { ImageBackground, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [title, onChangeTitle] = useState('');
+  const [showTitle, setShowTitle] = useState(false);
+
+  const isShowTitle = () => {
+    if(title) {
+      setShowTitle(true);
+    } else {
+      setShowTitle(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require('./assets/background.jpg')}
+        style={styles.image}
+      />
+
+      <View style={styles.avatar}>
+        <Image />
+
+        {showTitle &&
+          <Text>{title}</Text>
+        }
+
+        <Input onChangeText={onChangeTitle} value={title} />
+        <Button onPress={isShowTitle} title="OK" />
+        <StatusBar style="auto" />
+      </View>
+
+
     </View>
   );
 }
@@ -14,8 +43,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    height: '100%'
+  },
+  avatar: {
+    position: 'absolute',
+    marginTop: 80,
+    left: 0,
+    right: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
 });
